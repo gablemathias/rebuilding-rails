26ffecdfa18805fbac914163a7b1ca9d3f1cba49
Gabriel Mathias <gablemathias@icloud.com>
2023-09-22 13:15:07 -0300

    feat: add simple debug test on application

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,6 +7,7 @@ module Rulers
   
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
       ["Hello from Ruby on Rulers! Did you see how amazing it is?"]]
     end
