a7a3a4a48dc393d77f2b9953da5dfd715b7dd968
Gabriel Mathias <gablemathias@icloud.com>
2023-09-23 08:42:44 -0300

    fix: returned array with text info from the controller

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -16,9 +16,8 @@ module Rulers
 
       status = 200
       headers = {'Content-Type' => 'text/html'}
-      body = ["Hello from Ruby on Rulers! Did you see how amazing it is?"]
 
-      [status, headers, body]
+      [status, headers, text]
     end
   end
 
