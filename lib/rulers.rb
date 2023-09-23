abaca0858596cd39c95f90dfd75f8c508a69f919
Gabriel Mathias <gablemathias@icloud.com>
2023-09-23 08:09:43 -0300

    feat: create Controller class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,19 +2,33 @@
 
 require_relative "rulers/version"
 require "rulers/array.rb"
+require "rulers/routing"
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      # `echo debug > debug.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
 
       status = 200
       headers = {'Content-Type' => 'text/html'}
       body = ["Hello from Ruby on Rulers! Did you see how amazing it is?"]
 
       [status, headers, body]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
