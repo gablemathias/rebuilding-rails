a536f3165d7ef3594e6dc5e0157cb421fbb84545
Gabriel Mathias <gablemathias@icloud.com>
2023-09-25 19:23:10 -0300

    feat: add hack to redirect homepage to quotes

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -13,9 +13,20 @@ module Rulers
         return [404, {"ContentType" => "text/html"}, []]
       end
 
+      if env['PATH_INFO'] == '/'
+        controller = QuotesController.new(env)
+        return [200, {'ContentType' => 'text/html'}, [controller.send('a_quote')]]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = [controller.send(act)]
+      text =
+      begin
+        [controller.send(act)]
+      rescue
+        return [500, {"ContentType" => "text/html"}, []]
+      end
+
 
       status = 200
       headers = {'Content-Type' => 'text/html'}
